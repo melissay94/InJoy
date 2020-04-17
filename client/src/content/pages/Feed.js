94bdced11d4de4b15f8b890c77d602422745d80e
deanjheikkila <deanjheikkila@gmail.com>
2020-04-16 17:05:43 -0700

    reads in data from api

--- client/src/content/pages/Feed.js
+++ client/src/content/pages/Feed.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function Feed(props) {
+
     return (
         <div>Here be posts on the feed</div>
     )
--- client/src/content/pages/Prompts.js
+++ client/src/content/pages/Prompts.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function Prompts(props) {
+    const [prompts, setPrompts] = useState([]);
+
+    useEffect(() => {
+        // for (let i=0; i < 5; i++){
+            axios.get(`http://www.boredapi.com/api/activity/`)
+            .then(response => {
+                console.log("DAT DATA", response.data)
+        //         if (!prompts.includes(response.data.activity)) {
+                    setPrompts([response.data.activity]);
+        //         } else {
+        //             setPrompts(["yo", "yo", "yo", "yo", "yo"]);
+        //         }
+            }).catch(err=>{console.log(err)})
+        // }
+    }, [])
+
+    let promptsList = (prompts.length < 1) ? "" : prompts.map(prompt => {
+        return <div>{prompt}</div>
+    })
+    
     return (
-        <div>Here be a list of prompts</div>
+        <div>Here be a list of prompts: {promptsList}</div>
     )
 }
