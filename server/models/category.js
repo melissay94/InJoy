b602739943a993702e93c0d62df28a14e30f7ae2
melissay94 <melissay94@yahoo.com>
2020-05-15 13:29:08 -0700

    Relationships added for category

--- server/models/category.js
+++ server/models/category.js
@@ -1,10 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 120],
+          msg: "Invalid name length. Must not exceed 120 characters."
+        }
+      }
+    }
   }, {});
   Category.associate = function(models) {
     // associations can be defined here
+    Category.belongsToMany(models.User, { through: "UserCategory" });
+    Category.hasMany(models.Prompt);
   };
   return Category;
 };
