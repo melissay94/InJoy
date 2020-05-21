3ebc157a752514502eeb08397667babd1dd5088b
melissay94 <melissay94@yahoo.com>
2020-05-21 14:51:50 -0700

    Added schema

--- server/schema.js
+++ server/schema.js
@@ -3,15 +3,34 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
 type Query {
-
+  currentUser: User
+  user(id: Int!): User
 }
 
 type Mutation {
-
+  followUser(id: Int!): User
+  
 }
 
 # Custom types
+type User {
+  id: Int!
+  username: String!
+  email: String!
+  password: String!
+  name: String,
+  profileImage: String,
+  currentPrompt: # Prompt
+  promptExpiration: String,
+  hasPosted: Boolean,
+  comments: #[Comment!]!
+  followers: [User!]!
+  categories: #[Category!]!
+  postsCreated: #[Post!]!
+  postsLiked: #[Post!]!
+}
 
+# Authpayload
 
 `;
 
