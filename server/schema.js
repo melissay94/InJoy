ad61717cd31c1ae1995ee59446b99a3765fc65e6
melissay94 <melissay94@yahoo.com>
2020-05-26 12:15:25 -0700

    Completed schema parts for user and posts

--- server/schema.js
+++ server/schema.js
@@ -5,11 +5,21 @@ const typeDefs = gql`
 type Query {
   currentUser: User
   user(id: Int!): User
+  posts: [Post!]!
+  post(id: Int!): Post
 }
 
 type Mutation {
   followUser(id: Int!): User
-  
+  signUp(username: String!, email: String!, password: String!, name: String, profileImage: String): AuthPayload
+  login(email: String!, password: String!): AuthPayload
+  editCurrentUser(username: String, email: String, password: String, name: String, profileImage: String): AuthPayload
+  deleteCurrentUser: Boolean
+  editPost(id:Int!, title: String, description: String): Post
+  deletePost(id: Int!): Boolean
+  addCommentToPost(id: Int!, comment: Comment!): Comment
+  addLikeToPost(id: Int!): Boolean
+  removeLikeFromPost(id: Int!): Boolean
 }
 
 # Custom types
@@ -26,11 +36,24 @@ type User {
   comments: #[Comment!]!
   followers: [User!]!
   categories: #[Category!]!
-  postsCreated: #[Post!]!
-  postsLiked: #[Post!]!
+  postsCreated: [Post!]!
+  postsLiked: [Post!]!
 }
 
 # Authpayload
+type AuthPayload{
+  user: User
+  token: String
+}
+
+type Post {
+  id: Int!
+  title: String!
+  prompt: Prompt!
+  user: User!
+  image: String
+  description: String
+}
 
 `;
 
